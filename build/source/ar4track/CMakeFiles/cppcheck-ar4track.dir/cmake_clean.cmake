3cb47a2bac9fdab83ffd0588637633c076533cbf
Filip Szeliga <github@szeliga.org>
2017-02-11 18:14:09 +0100

    removed buold dir from master

--- build/source/ar4track/CMakeFiles/cppcheck-ar4track.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cppcheck-ar4track"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cppcheck-ar4track.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/source/ar4track_app/CMakeFiles/cppcheck-ar4track_app.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cppcheck-ar4track_app"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cppcheck-ar4track_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
